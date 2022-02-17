aec6470a2d1c681533dc48bcc33fe1ea158d8a98
PhredE <phrede@PhredEs-MacBook-Air.local>
2022-02-16 19:57:23 -0800

    finished models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,59 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
+
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
+
+class dog(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
+    # changes to instance methods do not require re-generation / running of migrations
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
+
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    # Create a dog_id FK
+    dog = models.ForeignKey(dog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    # change the default sort
+    class Meta:
+        ordering = ['-date']
+
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    dog = models.ForeignKey(dog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for dog _id: {self.dog_id} @{self.url}"
