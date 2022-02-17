d355b4af3f06b76d874860d12d183b57763c86fa
PhredE <phrede@PhredEs-MacBook-Air.local>
2022-02-16 19:58:56 -0800

    finished about, base, home

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cat, Feeding
+from .models import dog, Feeding
 
-admin.site.register(Cat)
+admin.site.register(dog)
 admin.site.register(Feeding)
